57ab089d9f8d98f568c74e49dd12e5719f5cd8e5
chen <361196961@qq.com>
2017-11-25 10:20:24 +0800

    动态加载components models

--- src/index.js
+++ src/index.js
@@ -27,8 +27,8 @@ app.use(createLoading());
 
 // 3. Model
 // app.model(require('./models/example'));
-app.model(require('./models/users'));
-
+// app.model(require('./models/user'));
+//  上述在 陆游中实现了动态的加载
 // 4. Router
 app.router(require('./router'));
 
--- src/router.js
+++ src/router.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
-import IndexPage from './routes/IndexPage';
 
-import Users from './routes/Users.js';
+// import IndexPage from './routes/IndexPage';
+// import Users from './routes/Users.js';
+// import Name from './routes/Name.js';
 
-import Name from './routes/Name.js';
 
-function RouterConfig({ history }) {
+import dynamic from 'dva/dynamic';
+
+function RouterConfig({ history, app }) {
+  
+  const IndexPage = dynamic({
+    app,
+    models: () => [import('./models/example')],
+    component: () => import('./routes/IndexPage')
+  })
+
+  const Users = dynamic({
+    app,
+    models: () => [import('./models/user')],
+    component: () => import('./routes/Users')
+  })
+
+  const Name = dynamic({
+    app,
+    component: () => import('./routes/Name')
+  })
+
   return (
     <Router history={history}>
       <Switch>
